4ffef4b4f6dd75d85fb60f3fff38e3c208abfc8e
axperien <axperien@yandex.ru>
2018-09-05 22:53:42 +0400

    add event onAdd

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,16 +5,26 @@ import AddTask from 'components/AddTask';
 export default class App extends React.Component {
     constructor(props){
         super(props);
+        this.state = {
+            data: this.props.data
+        }
+        this.onAdd = this.onAdd.bind(this)
+    }
+
+    onAdd = (item) => {
+        let newData = this.state.data;
+        newData.push(item);
+        this.setState({data: newData});
     }
 
     render() {
-        const { data } = this.props;
+        const { data } = this.state;
 
         return (
             <div className="container">
                 <h1 style={{marginBottom: '30px'}}>Список задач</h1>
                 {<TaskList data={data} />}
-                {<AddTask data={data} />}
+                {<AddTask data={data} onAdd={item => this.onAdd(item)} />}
             </div>
         )
     }
